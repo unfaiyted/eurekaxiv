57036be106425d991fd8bcc7c8bbfd070eda2fcd
unfaiyted <unfaiyted@gmail.com>
2018-03-20 12:29:31 -0500

    file moved to correct location, update to json file to remove code from being in website content page.

--- js/eureka.js
+++ js/eureka.js
@@ -3,30 +3,6 @@
 
     // Convert current time to Eorzea time return
     function eTimeCalc() {
-        // var d = new Date();
-        // var m = new Date();
-        // var sinceMidnight =  d - m.setHours(0,0,0,0);
-        //
-        //
-        // //console.log(sinceMidnight);
-        //
-        // var minutes = Math.floor(sinceMidnight / 60000);
-        // var seconds = ((sinceMidnight % 60000) / 1000).toFixed(0);
-        // //console.log( minutes + ":" + (seconds < 10 ? '0' : '') + seconds);
-        //
-        // var totalSeconds = (minutes*60)+seconds;
-        // var eMinutes = (totalSeconds/2.916666);
-        //
-        // console.log(eMinutes);
-        //
-        //
-        // var eMinute = Math.floor(eMinutes % 60);
-        // var totalHour = eMinutes / 60;
-        // var eHour = totalHour % 24;
-        //
-        //
-        // console.log(eHour + ':' + eMinutes);
-
 
         var localEpoch = (new Date()).getTime();
         var epoch = localEpoch * 20.571428571428573;
@@ -45,7 +21,6 @@
         });
     }
 
-    var runTime = eTimeCalc();
     var run = setInterval(updateTimer, 1000);
 
 
